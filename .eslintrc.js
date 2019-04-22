14e9e89ff12473ccad20565827cbff5071824291
Bonflintstone <freddy@railslove.com>
2019-04-22 12:22:43 +0200

    More things

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ["eslint:all", "plugin:vue/recommended", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "vue/max-attributes-per-line": "off",
+    "vue/require-default-prop": "off",
+    "vue/attribute-hyphenation": "off",
+    "sort-keys": "off",
+    "multiline-comment-style": "off",
+    "capitalized-comments": "off",
+    "one-var": "off",
+    "sort-imports": "off",
+    "no-magic-numbers": "warn",
+    "no-console": "off",
+    eqeqeq: ["error", "smart"],
+    "no-eq-null": "off"
+  }
+};
