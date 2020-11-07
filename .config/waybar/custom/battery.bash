a35c056eea16b2c57697040b96f4d7e243e1a8db
Bonflintstone <freddy@railslove.com>
2020-11-07 16:10:44 +0100

    Neater Sway bar, back to three screens

---
+++ .config/waybar/custom/battery.bash
@@ -0,0 +1,13 @@
+#! /bin/env bash
+
+if acpi | grep --quiet Discharging; then
+  EMOJI='🔋'
+else
+  EMOJI='🔌'
+fi
+PERCENTAGE=$(acpi | cut -d ',' -f 2 | xargs)
+
+TEXT="$EMOJI $PERCENTAGE"
+TOOLTIP=$(acpi | cut -d " " -f 3-)
+
+echo "{ \"text\": \"$TEXT\", \"tooltip\": \"$TOOLTIP\" }"
