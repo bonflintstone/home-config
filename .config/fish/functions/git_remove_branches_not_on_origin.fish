b9d020cd9a4fb6db9b8297babfa8b8cd8a063da3
Bonflintstone <freddy@railslove.com>
2021-04-16 11:21:38 +0200

    Adds new things

--- .config/fish/functions/git_remove_branches_not_on_origin.fish
+++ .config/fish/functions/git_remove_branches_not_on_origin.fish
@@ -1,5 +1,6 @@
+# Defined interactively
 function git_remove_branches_not_on_origin
 git remote prune origin
-git switch master
-git branch | ack -v (git branch -a | ack 'origin' | ack -v '\->' | sed 's/.*\/origin\///' | paste -sd '|') | xargs -r git branch -D '{}'
+git switch (git remote show origin | ag HEAD\ branch | cut -d ' ' -f5)
+git branch | ag -v (git branch -a | ag 'origin' | ag -v '\->' | sed 's/.*\/origin\///' | paste -sd '|') | xargs -r git branch -D '{}'
 end
