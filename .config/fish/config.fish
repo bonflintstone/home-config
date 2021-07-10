c6dd95e5069d8a19dba1eea81d04802996616324
Bonflintstone <freddy@railslove.com>
2021-07-10 10:09:41 +0200

    We are on mac now.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,2 @@
-
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /home/bonflintstone/Projects/code4maus/node_modules/tabtab/.completions/serverless.fish ]; and . /home/bonflintstone/Projects/code4maus/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /home/bonflintstone/Projects/code4maus/node_modules/tabtab/.completions/sls.fish ]; and . /home/bonflintstone/Projects/code4maus/node_modules/tabtab/.completions/sls.fish
-# tabtab source for slss package
-# uninstall by removing these lines or running `tabtab uninstall slss`
-[ -f /home/bonflintstone/Projects/code4maus/node_modules/tabtab/.completions/slss.fish ]; and . /home/bonflintstone/Projects/code4maus/node_modules/tabtab/.completions/slss.fish
+# rbenv
+status --is-interactive; and source (rbenv init -|psub)
