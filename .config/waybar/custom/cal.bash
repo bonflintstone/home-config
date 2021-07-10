06cf5c88d874bda047921b74fee9034d5b7cc17f
Bonflintstone <freddy@railslove.com>
2021-07-10 10:09:41 +0200

    We are on mac now.

--- .config/waybar/custom/cal.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/env bash
-
-COUNT=$(expr $(khal list -of a today eod | wc -l) - 1)
-LIST=$(khal list -of '{start-time}-{end-time} {title}' today tomorrow | sed '{:q;N;s/\n/\\n/g;t q}' | sed 's/"//g')
-
-echo "{ \"text\": \"$COUNT\", \"tooltip\": \"$LIST\" }"
