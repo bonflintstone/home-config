b698bae401370b0a81951b8e6010e98f009004fd
Bonflintstone <freddy@railslove.com>
2021-10-29 13:58:26 +0200

    Better things!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,4 @@
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
+
+source /opt/homebrew/opt/asdf/asdf.fish
--- .config/fish/functions/git_remove_branches_not_on_origin.fish
+++ .config/fish/functions/git_remove_branches_not_on_origin.fish
@@ -1,6 +1,5 @@
-# Defined interactively
 function git_remove_branches_not_on_origin
 git remote prune origin
 git switch (git remote show origin | ag HEAD\ branch | cut -d ' ' -f5)
-git branch | ag -v (git branch -a | ag 'origin' | ag -v '\->' | sed 's/.*\/origin\///' | paste -sd '|') | xargs -r git branch -D '{}'
+git branch | ag -v (git branch -a | ag 'remotes/origin' | ag -v '\->' | sed 's/.*\/origin\///' | paste -sd '|' /dev/stdin) | xargs -r git branch -D
 end
